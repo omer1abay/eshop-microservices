31a822d09c7c8b6e342936b5c9338b36bc477cf3
omer1abay <abayomer1905@gmail.com>
2024-10-11 23:50:39 +0300

    Update - GetProductById and ByCategory operations added

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Product Product) : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(bool IsSuccess);
+
+internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Command}", command);
+
+        var productOld = await session.LoadAsync<Product>(command.Product.Id, cancellationToken);
+
+        if (productOld is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        productOld = command.Product;
+
+        session.Update<Product>(productOld);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateProductResult(true);
+
+    }
+}
