00691316cd10b86c16350cd881c0deb732541607
omer1abay <abayomer1905@gmail.com>
2024-10-27 18:30:16 +0300

    Cache-Aside pattern implementing

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,42 @@
+﻿//These usings are using only in this class
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data;
+
+//In this section CachedBasketRepository acts as a proxy because we use it as an intermediate class to access the main basketrepository class 
+
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var cacheBasket = await cache.GetStringAsync(userName, cancellationToken);
+
+        if (!string.IsNullOrEmpty(cacheBasket))
+            return JsonSerializer.Deserialize<ShoppingCart>(cacheBasket)!;
+
+        var basket = await repository.GetBasket(userName, cancellationToken);
+
+        await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellationToken = default)
+    {
+        await repository.StoreBasket(cart, cancellationToken);
+
+        await cache.SetStringAsync(cart.UserName, JsonSerializer.Serialize(cart), cancellationToken);
+
+        return cart;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        await repository.DeleteBasket(userName, cancellationToken);
+
+        await cache.RemoveAsync(userName, cancellationToken);
+        
+        return true;
+    }
+}
