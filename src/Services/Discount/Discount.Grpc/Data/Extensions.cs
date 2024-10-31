d03f1ca3306f6e8fc9ef3a0679c4122051391434
omer1abay <abayomer1905@gmail.com>
2024-10-31 23:51:18 +0300

    Sqlite db configuration has done

---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace Discount.Grpc.Data;
+
+//Automating migration
+public static class Extensions
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+
+        //auto migrate
+        dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
