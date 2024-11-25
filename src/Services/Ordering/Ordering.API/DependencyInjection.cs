6cf1ef69c3ad0e2494cc860a979fc09229616b2b
omer1abay <abayomer1905@gmail.com>
2024-11-25 23:16:59 +0300

    DI container added as extension methods

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    //DI container
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        //services.AddCarter();
+
+        return services;
+    }
+
+    //Middleware
+    public static WebApplication UseApiServices(this WebApplication app) 
+    { 
+        //app.MapCarter();
+        
+
+        return app;  
+    }
+
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+//All clean arch layers di has added as extension
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 var app = builder.Build();
 
